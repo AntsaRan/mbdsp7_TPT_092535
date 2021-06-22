3e1f861e7ec1d9836ab73b34e39f93b770940da1
Chad <thongsangchad@gmail.com>
2021-06-22 13:33:49 +0100

    Ajout de la page Achat

---
+++ Android/app/src/main/java/com/example/bet/ui/Achat_Fragment.java
@@ -0,0 +1,63 @@
+package com.example.bet.ui;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.Display;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.bet.R;
+import com.google.zxing.WriterException;
+import com.google.zxing.integration.android.IntentIntegrator;
+
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
+import androidmads.library.qrgenearator.QRGSaver;
+
+import static android.content.Context.WINDOW_SERVICE;
+
+public class Achat_Fragment extends Fragment  {
+    Button scanBtn;
+    TextView messageText, messageFormat;
+    private Context mContext;
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.fragment_achat, container,false);
+        scanBtn=view.findViewById(R.id.button_scan);
+        scanBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                IntentIntegrator intentIntegrator = new IntentIntegrator(getActivity());
+                intentIntegrator.setPrompt("Scan a barcode or QR Code");
+                intentIntegrator.setOrientationLocked(true);
+                intentIntegrator.initiateScan();
+            }
+        });
+        return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mContext = context;
+    }
+
+
+
+}
