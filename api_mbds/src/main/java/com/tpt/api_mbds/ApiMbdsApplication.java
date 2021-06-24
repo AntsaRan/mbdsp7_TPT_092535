cfbc64bfd2d7f89b53ebf3213c6625bd338f4297
Chad <thongsangchad@gmail.com>
2021-06-24 11:54:03 +0100

    new project

---
+++ api_mbds/src/main/java/com/tpt/api_mbds/ApiMbdsApplication.java
@@ -0,0 +1,13 @@
+package com.tpt.api_mbds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMbdsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiMbdsApplication.class, args);
+    }
+
+}
---
+++ api_mbds/src/test/java/com/tpt/api_mbds/ApiMbdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpt.api_mbds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMbdsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
