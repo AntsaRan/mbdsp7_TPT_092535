e3b601e55276526791ec480b318e6351c6601f83
Chad <thongsangchad@gmail.com>
2021-06-24 10:56:47 +0100

    Création du projet spring

---
+++ API_TPT/src/main/java/com/tpt/API_TPT/ApiTptApplication.java
@@ -0,0 +1,13 @@
+package com.tpt.API_TPT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTptApplication.class, args);
+	}
+
+}
---
+++ API_TPT/src/test/java/com/tpt/API_TPT/ApiTptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpt.API_TPT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
