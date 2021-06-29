a18c552eb87924af0143452dc921e6723fa0ece7
Antsa <vranari@yahoo.fr>
2021-06-29 17:31:37 +0300

    services

--- betproject/betproject/src/app/shared/services/match-service.service.ts
+++ betproject/betproject/src/app/shared/services/match-service.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Match } from '../models/match.model';
+import { HttpClient,HttpHeaders  } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MatchServiceService {
 
-  constructor() { }
+  uri = "http://localhost:3000/getAllMatches";
+
+  constructor(private http:HttpClient) { }
+  
+  getMatches(): Observable<Match[]> {
+    return this.http.get<Match[]>(this.uri);
+  }
 }
