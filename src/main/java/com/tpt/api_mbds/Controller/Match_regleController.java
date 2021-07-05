52a111a0f896126a482d6a956e6eafc12d83f8f2
Chad <thongsangchad@gmail.com>
2021-07-05 13:00:30 +0300

    Ajout des API pour mobile

--- src/main/java/com/tpt/api_mbds/Controller/Match_regleController.java
+++ src/main/java/com/tpt/api_mbds/Controller/Match_regleController.java
@@ -1,7 +1,9 @@
 package com.tpt.api_mbds.Controller;
 
+import com.tpt.api_mbds.model.Match;
 import com.tpt.api_mbds.model.Match_regle;
 import com.tpt.api_mbds.repository.Match_regleRepository;
+import com.tpt.api_mbds.response.Response;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -59,5 +61,53 @@ public class Match_regleController {
            // return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
+    //MOBILE
+    @GetMapping("/matchRegles/getAll")
+    public ResponseEntity<Response<Match_regle>> getAllMatch_regleResult(){
+        try {
+            List<Match_regle> match_regle = new ArrayList<Match_regle>();
+            Response<Match_regle> result=new Response<>();
+
+            match_regleRepository.findAll().forEach(match_regle::add);
+
+            if (match_regle.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            else{
+                result.setPage(1);
+                result.setResults(match_regle);
+            }
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/matchRegle/{idMatch}")
+    public ResponseEntity<Response<Match_regle>> getMatch_regleByIdMatchResult(@PathVariable("idMatch") String idMatch) {
+        try {
+            System.out.println("ITO ILAY STRING "+idMatch);
+            ObjectId idMatchRecherche = new ObjectId(idMatch);
+            Match_regle match_regleData = match_regleRepository.findMatch_regleByIdMatch(idMatchRecherche);
+            Response<Match_regle> result=new Response<>();
+            System.out.println("ITO Raha nahita izy  "+match_regleData.getId());
+            List<Match_regle> matchList=new ArrayList<>();
+
+            if (match_regleData!=null) {
+                matchList.add(match_regleData);
+                result.setResults(matchList);
+                result.setPage(1);
+                return new ResponseEntity<>(result, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        }
+        catch (Exception e){
+            throw e;
+            // return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 
 }
--- src/main/java/com/tpt/api_mbds/Controller/RegleController.java
+++ src/main/java/com/tpt/api_mbds/Controller/RegleController.java
@@ -1,8 +1,10 @@
 package com.tpt.api_mbds.Controller;
 
 
+import com.tpt.api_mbds.model.Match;
 import com.tpt.api_mbds.model.Regle;
 import com.tpt.api_mbds.repository.RegleRepository;
+import com.tpt.api_mbds.response.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -47,6 +49,47 @@ public class RegleController {
 
             if (regleData.isPresent()) {
                 return new ResponseEntity<>(regleData.get(), HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        }
+        catch (Exception e){
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    //MOBILE
+    @GetMapping("/regles/getAll")
+    public ResponseEntity<Response<Regle>> getAllRegleResult(@RequestParam(required = false) String titre){
+        try {
+            List<Regle> regles = new ArrayList<Regle>();
+            Response<Regle> resultat=new Response<>();
+            if (titre == null)
+                regleRepository.findAll().forEach(regles::add);
+            else
+                regleRepository.findReglesByTitreContaining(titre).forEach(regles::add);
+
+
+            if (regles.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            resultat.setPage(1);
+            resultat.setResults(regles);
+            return new ResponseEntity<>(resultat, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @GetMapping("/regles/{id}")
+    public ResponseEntity<Response<Regle>> getRegleByIdResult(@PathVariable("id") String id) {
+        try {
+            Optional<Regle> regleData = regleRepository.findById(id);
+            Response<Regle> resultat=new Response<>();
+            List<Regle> regleList=new ArrayList<>();
+            if (regleData.isPresent()) {
+                regleList.add(regleData.get());
+                resultat.setResults(regleList);
+                resultat.setPage(1);
+                return new ResponseEntity<>(resultat, HttpStatus.OK);
             } else {
                 return new ResponseEntity<>(HttpStatus.NOT_FOUND);
             }
