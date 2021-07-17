e7099fdda609a98ee2fccbd450a25d6e7a70952c
Antsa <vranari@yahoo.fr>
2021-07-17 22:03:30 +0300

    Update auth.service.ts

--- betproject/betproject/src/app/shared/services/auth.service.ts
+++ betproject/betproject/src/app/shared/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { BehaviorSubject, Observable, of } from 'rxjs';
+import { BehaviorSubject, Observable, of, Subject } from 'rxjs';
 import { Parieur } from '../models/parieur.model';
 import { catchError, map } from 'rxjs/operators';
 
@@ -9,54 +9,73 @@ import { catchError, map } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class AuthService {
+
   loggedIn: boolean = false;
   admin = false;
   private currentUserSubject: BehaviorSubject<Parieur>;
   public currentUser: Observable<Parieur>;
+  private userLoggedIn = new Subject<boolean>();
 
   constructor(
     private http: HttpClient, private router: Router) {
+    this.userLoggedIn.next(false);
     this.currentUserSubject = new BehaviorSubject<Parieur>(JSON.parse(localStorage.getItem('currentUser')));
     this.currentUser = this.currentUserSubject.asObservable();
   }
 
-
-  uri = "http://localhost:8010/api/auth/";
+  uri = "http://localhost:8010/auth/";
 
   public get currentUserValue(): Parieur {
     return this.currentUserSubject.value;
   }
 
-  logIn(pseudo: string, passowrd: string) {
+  logIn(mail: string, passowrd: string): Observable<any> {
+    console.log("LOGIN O")
     let user = new Parieur();
-    user.pseudo = pseudo;
+    user.mail = mail;
     user.pwd = passowrd;
-    return this.http.post<any>(this.uri + "login", user)
+    console.log("user alefa " + user.mail)
+    return this.http.post<any>(this.uri + "/login", user)
       .pipe(
         map(user => {
-          if (user) {
+          if (user.user != null) {
+            console.log(user.user.id + " user id");
+            console.log(user.user.jetons + " user JETONS");
             console.log(user.token + "user token");
             this.setSession(user);
-            return user;
+            this.setUserLoggedIn(true);
+            return user.user;
+          } else {
+            return null;
           }
         }),
         catchError(this.handleError<any>('### catchError: login'))
       );
   }
-  private setSession(user) {
-    localStorage.setItem('currentUser', JSON.stringify(user.id));
-    localStorage.setItem('username', user.username);
-    localStorage.setItem('currentToken', user.token);
-    localStorage.setItem('isadmin', user.isadmin);
-    this.currentUserSubject.next(user);
+  setUserLoggedIn(userLoggedIn: boolean) {
+    this.userLoggedIn.next(userLoggedIn);
   }
+
+  getUserLoggedIn(): Observable<boolean> {
+    return this.userLoggedIn.asObservable();
+  }
+  private setSession(user) {
+    localStorage.setItem('currentUser', user.user.id);
+    localStorage.setItem('username', user.user.nom);
+    localStorage.setItem('prenom', user.user.prenom);
+    localStorage.setItem('mail', user.user.mail);
+    localStorage.setItem('currentToken', user.token);
+    localStorage.setItem('jetons', user.user.jetons);
+    this.currentUserSubject.next(user.user);
+  }
+
   private handleError<T>(operation: any, result?: T) {
-    console.log("HANDLE ERROR");
     return (error: any): Observable<T> => {
       console.log(error);
       console.log(operation + ' a échoué' + error.message);
       return of(result as T);
     }
   }
+
 }
 
