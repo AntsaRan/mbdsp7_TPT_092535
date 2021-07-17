0166f959b882a9bbfb383f7f495f81891b3a8128
Antsa <vranari@yahoo.fr>
2021-07-17 22:03:04 +0300

    mes paris

---
+++ betproject/betproject/src/app/mesParis/mesparis/mesparis.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mesparis',
+  templateUrl: './mesparis.component.html',
+  styleUrls: ['./mesparis.component.css']
+})
+export class MesparisComponent implements OnInit {
+  loading:boolean=false;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
