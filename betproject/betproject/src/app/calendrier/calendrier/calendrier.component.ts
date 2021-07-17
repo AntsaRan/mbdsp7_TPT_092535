d1bea941e3d4ce0968fa00ccbd5d4de9d49173a6
Antsa <vranari@yahoo.fr>
2021-07-17 21:59:56 +0300

    update calendrier

--- betproject/betproject/src/app/calendrier/calendrier/calendrier.component.ts
+++ betproject/betproject/src/app/calendrier/calendrier/calendrier.component.ts
@@ -1,5 +1,8 @@
 import { DatePipe } from '@angular/common';
+import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Match } from 'src/app/shared/models/match.model';
 import { CartService } from 'src/app/shared/services/cart.service';
 import { MatchServiceService } from 'src/app/shared/services/match-service.service';
 
@@ -9,21 +12,29 @@ import { MatchServiceService } from 'src/app/shared/services/match-service.servi
   styleUrls: ['./calendrier.component.css']
 })
 export class CalendrierComponent implements OnInit {
-
+  match: Match[] = [];
   loading: boolean = true;
   date: Date = null;
-
+  datereceived: Date = null;
   constructor(private datePipe: DatePipe,
     private cartserv: CartService,
-    private matchserv: MatchServiceService) { }
+    private matchserv: MatchServiceService,
+    private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    if (this.route.snapshot.params.date != null) {
+      this.datereceived = this.route.snapshot.params.date;
+      this.getMatchByDate();
+    }
   }
   getMatchByDate() {
-    console.log(this.date.toJSON());
-    this.matchserv.getMatchByDate(this.date)
-      .subscribe(matches => {
 
+    this.matchserv.getMatches()
+      .subscribe(matches => {
+        matches.forEach(match => {
+          this.match.push(match);
+        })
+        this.loading = false;
       })
 
   }
