8df09a27000b179ad1a4e053110441fe3d0c33b5
Antsa <vranari@yahoo.fr>
2021-07-17 22:04:56 +0300

    topparis

---
+++ betproject/betproject/src/app/top50/topparis/topparis.component.ts
@@ -0,0 +1,41 @@
+import { DatePipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Match } from 'src/app/shared/models/match.model';
+import { CartService } from 'src/app/shared/services/cart.service';
+import { MatchServiceService } from 'src/app/shared/services/match-service.service';
+
+@Component({
+  selector: 'app-topparis',
+  templateUrl: './topparis.component.html',
+  styleUrls: ['./topparis.component.css']
+})
+export class TopparisComponent implements OnInit {
+
+  matches: Match[]=[];
+  currentdate = new Date();
+  date: string;
+  loading:boolean=true;
+  constructor(
+    private datePipe: DatePipe,
+    private cartserv: CartService,
+    private matchserv: MatchServiceService) {
+
+    this.date = this.datePipe.transform(this.currentdate, 'yyyy-MM-dd');
+  }
+
+  ngOnInit(): void {
+    this.getMatches();
+  }
+  getMatches() {
+    console.log("getmatches");
+    this.matchserv.getMatches()
+      .subscribe(data => {
+        data.forEach(match=>{         
+          console.log(match.id+ " match")
+          this.matches.push(match);
+        })
+        this.loading=false;
+      });
+  }
+
+}
