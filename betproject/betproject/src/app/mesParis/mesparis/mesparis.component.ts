6779eddf4dea9eef4fb59c268762989ae81113f7
Antsa <vranari@yahoo.fr>
2021-07-19 16:03:09 +0300

    données dynamiques mes paris

--- betproject/betproject/src/app/mesParis/mesparis/mesparis.component.ts
+++ betproject/betproject/src/app/mesParis/mesparis/mesparis.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Match } from 'src/app/shared/models/match.model';
+import { Pari } from 'src/app/shared/models/pari.model';
+import { Parismatch } from 'src/app/shared/models/parismatch.model';
+import { MatchServiceService } from 'src/app/shared/services/match-service.service';
+import { ParisService } from 'src/app/shared/services/paris.service';
+import { InscriptionComponent } from 'src/app/inscription/inscription/inscription.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-mesparis',
@@ -6,10 +13,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mesparis.component.css']
 })
 export class MesparisComponent implements OnInit {
-  loading:boolean=false;
-  constructor() { }
+  loading: boolean = false;
+  paris: Parismatch[] = [];
+  nodata: string;
+  constructor(public dialog: MatDialog,private parisserv: ParisService, private matchserv: MatchServiceService) { }
 
   ngOnInit(): void {
+    if(localStorage.getItem('currentUser')){
+      console.log(localStorage.getItem('currentUser'));
+      this.getParisbyuser(localStorage.getItem('currentUser'));
+    }
+  }
+  getParisbyuser(id) {
+    let matchpari: Match;
+    this.parisserv.getPariByIdUser(id)
+      .subscribe(data => {
+        if (data) {
+          console.log(" daataaaa");
+          data.forEach(pari => {
+            console.log(pari.idMatch + " MISEEEEE")
+            this.matchserv.getMatchById(pari.idMatch)
+              .subscribe(match => {
+                if (match) {
+                  console.log(match.date);
+                  matchpari = match;
+                  let parimatch = new Parismatch();
+                  parimatch.id = pari.id;
+                  parimatch.date = pari.dateParis;
+                  parimatch.match = matchpari;
+                  parimatch.mise = pari.mise;
+                  this.paris.push(parimatch);
+                }
+
+              })
+          })
+        } else {
+          console.log("no daataaaa");
+          this.nodata = "No data";
+        }
+
+        this.loading = false;
+      });
   }
 
 }
