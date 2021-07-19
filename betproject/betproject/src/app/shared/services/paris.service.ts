6c70b6dc7f05f6f4fb429b967abb690e7284a084
Antsa <vranari@yahoo.fr>
2021-07-19 16:05:41 +0300

    Create paris.service.ts

---
+++ betproject/betproject/src/app/shared/services/paris.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient,HttpHeaders  } from '@angular/common/http';
+import { Pari } from '../models/pari.model';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ParisService {
+
+  uri = "http://localhost:8010/pari";
+   // uri="https://apinode-mbds.herokuapp.com"
+
+  constructor(private http:HttpClient) { }
+  
+  insertparis(pari:Pari):Observable<any>{
+    console.log( "insert pari service " + pari.idMatch);
+    return this.http.post(this.uri+"/insertpari",pari);
+  }
+
+  getPariByIdUser(id): Observable<Pari[]> {
+    return this.http.get<Pari[]>(this.uri+"/getparisuser/"+id);
+  }
+}
