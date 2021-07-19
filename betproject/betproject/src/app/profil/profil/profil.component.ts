1e033f8295b487093392d8d19060f980261777b7
Antsa <vranari@yahoo.fr>
2021-07-19 16:03:53 +0300

    show profil

--- betproject/betproject/src/app/profil/profil/profil.component.ts
+++ betproject/betproject/src/app/profil/profil/profil.component.ts
@@ -8,28 +8,29 @@ import { Parieur } from 'src/app/shared/models/parieur.model';
   styleUrls: ['./profil.component.css']
 })
 export class ProfilComponent implements OnInit {
-  loading:boolean=true;
-  user:Parieur=new Parieur();
-  constructor( private router: Router) { }
+  loading: boolean = true;
+  user: Parieur = new Parieur();
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
-    if(localStorage.getItem('currentUser')!=null){
-      this.loading=false;
-      this.user.id=localStorage.getItem('currentUser');
-      this.user.jetons=Number.parseInt(localStorage.getItem('jetons'));
-      this.user.mail=localStorage.getItem('mail');
-      this.user.prenom=localStorage.getItem('prenom');
-      this.user.nom=localStorage.getItem('username');
-    }else{
+    if (localStorage.getItem('currentUser') != null) {
+      console.log(localStorage.getItem('currentUser') + " GGGGGGGGGGGGGGG");
+      this.loading = false;
+      this.user.id = localStorage.getItem('currentUser');
+      this.user.jetons = Number.parseInt(localStorage.getItem('jetons'));
+      this.user.mail = localStorage.getItem('mail');
+      this.user.prenom = localStorage.getItem('prenom');
+      this.user.nom = localStorage.getItem('username');
+    } else {
       this.reloadComponent();
     }
   }
   reloadComponent() {
-          console.log(localStorage.getItem('jetons') + " GGGGGGGGGGGGGGG");
-
+    console.log(localStorage.getItem('jetons') + " jetons");
+    this.router.navigate(["/"]);
     /* 
      this.router.routeReuseStrategy.shouldReuseRoute = () => false;
      this.router.onSameUrlNavigation = 'reload';*/
-     //this.router.navigate(["/"]); 
-   }
+    //
+  }
 }
