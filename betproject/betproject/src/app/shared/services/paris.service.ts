9358f49d7d8144434133b83d06a5fc6e04786a66
Antsa <vranari@yahoo.fr>
2021-07-22 21:38:09 +0300

    Update paris.service.ts

--- betproject/betproject/src/app/shared/services/paris.service.ts
+++ betproject/betproject/src/app/shared/services/paris.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient,HttpHeaders  } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Pari } from '../models/pari.model';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
@@ -10,16 +10,16 @@ import { Observable } from 'rxjs';
 export class ParisService {
 
   uri = "http://localhost:8010/pari";
-   // uri="https://apinode-mbds.herokuapp.com"
+  //  uri="https://apinode-mbds.herokuapp.com/pari"
 
-  constructor(private http:HttpClient) { }
-  
-  insertparis(pari:Pari):Observable<any>{
-    console.log( "insert pari service " + pari.idMatch);
-    return this.http.post(this.uri+"/insertpari",pari);
+  constructor(private http: HttpClient) { }
+
+  insertparis(pari: Pari): Observable<any> {
+    console.log("insert pari service " + pari.idMatch);
+    return this.http.post(this.uri + "/insertpari", pari);
   }
 
   getPariByIdUser(id): Observable<Pari[]> {
-    return this.http.get<Pari[]>(this.uri+"/getparisuser/"+id);
+    return this.http.get<Pari[]>(this.uri + "/getparisuser/" + id);
   }
 }
