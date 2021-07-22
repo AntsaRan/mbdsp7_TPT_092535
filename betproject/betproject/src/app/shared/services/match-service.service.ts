96c84318548d084c1f9fa073270fb00ae4532cb9
Antsa <vranari@yahoo.fr>
2021-07-22 21:37:49 +0300

    Update match-service.service.ts

--- betproject/betproject/src/app/shared/services/match-service.service.ts
+++ betproject/betproject/src/app/shared/services/match-service.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { MatchRegles } from '../models/matchregles.model';
 import { Match } from '../models/match.model';
-import { HttpClient,HttpHeaders  } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Equipe } from '../models/equipe.model';
 
 @Injectable({
@@ -11,30 +11,30 @@ import { Equipe } from '../models/equipe.model';
 export class MatchServiceService {
 
   uri = "http://localhost:8010";
-   // uri="https://apinode-mbds.herokuapp.com"
+  // uri="https://apinode-mbds.herokuapp.com"
 
-  constructor(private http:HttpClient) { }
-  
+  constructor(private http: HttpClient) { }
+
   getMatches(): Observable<Match[]> {
-    return this.http.get<Match[]>(this.uri+"/getAllMatches");
+    return this.http.get<Match[]>(this.uri + "/getAllMatches");
   }
   getMatchById(id): Observable<Match> {
-    return this.http.get<Match>(this.uri+"/match/"+id);
+    return this.http.get<Match>(this.uri + "/match/" + id);
   }
 
-  getMatchByEquipe(id):Observable<Match[]>{
-    return this.http.get<Match[]>(this.uri+"/match/equipe/"+id);
+  getMatchByEquipe(id): Observable<Match[]> {
+    return this.http.get<Match[]>(this.uri + "/match/equipe/" + id);
   }
 
-  getMatchRegles(id):Observable<MatchRegles>{
-    return this.http.get<MatchRegles>(this.uri+"/matchregles/"+id);
+  getMatchRegles(id): Observable<MatchRegles> {
+    return this.http.get<MatchRegles>(this.uri + "/matchregles/" + id);
   }
-  
-  getMatchByDate(date):Observable<Match[]>{
-    return this.http.get<Match[]>(this.uri+"/match/date/"+date);
+
+  getMatchByDate(date): Observable<Match[]> {
+    return this.http.get<Match[]>(this.uri + "/match/date/" + date);
   }
-  
-  getMatchBymaxParis(date:Date):Observable<Match[]>{
-    return this.http.get<Match[]>(this.uri+"/match/paris/"+date);
+
+  getMatchBymaxParis(date: Date): Observable<Match[]> {
+    return this.http.get<Match[]>(this.uri + "/match/paris/" + date);
   }
 }
