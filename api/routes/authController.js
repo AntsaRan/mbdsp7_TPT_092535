3ca5f7b140ef795e51594182074acb946212f81c
Antsa <vranari@yahoo.fr>
2021-07-26 20:08:26 +0300

    Update authController.js

--- api/routes/authController.js
+++ api/routes/authController.js
@@ -4,18 +4,19 @@ var router = express.Router();
 var bodyParser = require('body-parser');
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
-//var VerifyToken = require('./VerifyToken');
-//var jwt = require('jsonwebtoken');
-//var bcrypt = require('bcryptjs');
-//var config = require('../config');
+
 let auth = require('./auth')
 
-  router.post('/login', function(req, res) {
-    auth.loginUser(req,res);
-  });
+router.post('/login', function (req, res) {
+  console.log("tody ato login");
+  auth.loginUser(req, res);
+});
 
-  router.get('/logout', function(req, res) {
-    res.status(200).send({ auth: false, token: null });
-  });
-
-  module.exports = router;
+router.get('/logout', function (req, res) {
+  res.status(200).send({ auth: false, token: null });
+});
+router.post('/sign', function (req, res) {
+  console.log("tody ato inscription");
+  auth.sign(req, res);
+});
+module.exports = router;
