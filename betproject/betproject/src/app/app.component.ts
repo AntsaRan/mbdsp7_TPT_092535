c9f81c183c9b429817f26c78b4a350a7b9e527d7
Antsa <vranari@yahoo.fr>
2021-07-29 01:41:02 +0300

    notifications

--- betproject/betproject/src/app/app.component.ts
+++ betproject/betproject/src/app/app.component.ts
@@ -7,6 +7,9 @@ import { LoginComponent } from './login/login/login.component';
 import { InscriptionComponent } from './inscription/inscription/inscription.component';
 import { MatchServiceService } from './shared/services/match-service.service';
 import { Router } from '@angular/router';
+import { MessagingService } from './shared/services/messaging.service';
+import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
+import { Notifs } from './shared/models/notifs';
 
 @Component({
   selector: 'app-root',
@@ -17,20 +20,45 @@ export class AppComponent implements OnInit {
   title = 'betproject';
   islogged: boolean = false;
   nomutilisateur: string = "";
+  searchval: string = "";
+  nbnotif: number = 0;
+  hidden = false;
+  horizontalPosition: MatSnackBarHorizontalPosition = 'start';
+  verticalPosition: MatSnackBarVerticalPosition = 'bottom';
+  notif:any[]=[];
   constructor(
     private datePipe: DatePipe,
     private cartserv: CartService,
     public dialog: MatDialog,
-    private router: Router) {
+    private router: Router, private mess: MessagingService,private _snackBar: MatSnackBar) {
 
   }
   ngOnInit(): void {
-
     if (localStorage.getItem('currentUser') != null) {
       this.islogged = true;
       this.nomutilisateur = localStorage.getItem('username');
     }
+    this.mess.receiveMessage();
+    this.mess.msg$.subscribe(m => {
+      if (m) {
+        this.nbnotif += 1;
+        var msg: any = m;
+        this.notif.push(msg.data);
+        console.log(JSON.stringify(msg.data) + " m appcompo 37");
+      }
+    })
   }
+  toggleBadgeVisibility(){
+    if(this.nbnotif==0){
+      this.hidden = !this.hidden;
+      this.notif=[];
+    }else{
+      this.hidden = !this.hidden;
+      this.nbnotif=0;
+    }
+  
+  }
+
   openLoginDialog() {
     const dialogRef = this.dialog.open(LoginComponent);
     dialogRef.afterClosed().subscribe(result => {
@@ -50,7 +78,12 @@ export class AppComponent implements OnInit {
   reloadComponent() {
     window.location.reload();
     //this.router.navigate(["/"]);
-   
+
+  }
+  onSubmitsearch(event) {
+    console.log(this.searchval + " searchval ");
+    //this.router.navigate(["/search?search="+this.searchval]);
+    this.router.navigate(['/search'], { queryParams: { search: this.searchval } });
 
   }
 }
