813aa6f146bcfccfeb917cc23e0abe453e05ca95
Antsa <vranari@yahoo.fr>
2021-07-29 01:49:34 +0300

    Update environment.ts

--- betproject/betproject/src/environments/environment.ts
+++ betproject/betproject/src/environments/environment.ts
@@ -3,9 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
-
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB1XqvkjClNswR56VveJGcu6DWTKc7hGco",
+    authDomain: "mbds-5e91b.firebaseapp.com",
+    projectId: "mbds-5e91b",
+    storageBucket: "mbds-5e91b.appspot.com",
+    messagingSenderId: "1003946694596",
+    appId: "1:1003946694596:web:7178143c417a66c740f691",
+    measurementId: "G-KNQR6ERP5Q"
+  }
+}
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
