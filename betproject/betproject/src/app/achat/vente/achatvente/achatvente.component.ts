e3974202f72e8b8b16ead3a054da677aebda5c54
Antsa <vranari@yahoo.fr>
2021-07-30 15:41:44 +0300

    achat vente

--- betproject/betproject/src/app/achat/vente/achatvente/achatvente.component.ts
+++ betproject/betproject/src/app/achat/vente/achatvente/achatvente.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 import { Const } from 'src/app/shared/const/const';
 import { Jeton } from 'src/app/shared/models/jeton';
 import { Parieur } from 'src/app/shared/models/parieur.model';
+import { AuthService } from 'src/app/shared/services/auth.service';
+import { JetonsService } from 'src/app/shared/services/jetons.service';
 @Component({
   selector: 'app-achatvente',
   templateUrl: './achatvente.component.html',
@@ -10,22 +14,33 @@ import { Parieur } from 'src/app/shared/models/parieur.model';
 export class AchatventeComponent implements OnInit {
   amontant: string = "";
   ajetons: string = "";
-  user:Parieur=new Parieur();
+  user: Parieur = new Parieur();
   vjetons: string = "";
   error: string = "";
+  errorvente:string="";
   prix: Number = Const.prixJeton;
-  mise
-  constructor() { }
+  mise;
+  solde;
+  constructor(private auth: AuthService, private jeton:JetonsService,
+    private router: Router, public dialog: MatDialog) { }
 
   ELEMENT_DATA: Jeton[] = [
     { devise: "Ariary", prix: this.prix }
   ];
   displayedColumns: string[] = ['devise', 'prix'];
   dataSource = this.ELEMENT_DATA;
-  ngOnInit(): void {
-    this.user=JSON.parse(localStorage.getItem('user'));
-    this.mise=localStorage.getItem('miseTotale');
 
+  ngOnInit(): void {
+    this.user = JSON.parse(localStorage.getItem('user'));
+    this.mise = localStorage.getItem('miseUser');
+    this.auth.getUserByID(this.user.id)
+      .subscribe(u => {
+        this.solde=u.jetons;
+      })
+      this.auth.getUserMise(this.user.id)
+      .subscribe(mise => {
+        this.mise = mise.toString();
+      })
   }
   changemontant() {
     console.log("montant: " + this.amontant + " prix: " + this.prix);
@@ -43,12 +58,17 @@ export class AchatventeComponent implements OnInit {
     // vérifier que champs non vides
     if (this.ajetons == "" || this.amontant == "") {
       this.error = "Tous les champs doivent être renseignés";
+      return;
     } else {
       if (Number(this.ajetons) < 0 || Number(this.amontant) < 0 || String(this.amontant).includes(".") || String(this.ajetons).includes(".")) {
         this.error = "Les valeurs doivent être des nombres entiers positifs"
-      }else{
-        this.error="";
-        console.log( "OK");
+        return;
+      } else {
+        this.jeton.achatjeton(this.user.id,this.ajetons)
+        .subscribe(m=>{
+          console.log("OK "+ m);
+          window.location.reload();
+        })
       }
     }
     // vérifier que nombre de jetons entier
