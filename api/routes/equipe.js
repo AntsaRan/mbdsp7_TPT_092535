8a160266ce3925bc8a25538c611e245d3a79e1f9
Antsa <vranari@yahoo.fr>
2021-07-30 15:41:14 +0300

    correct

--- api/routes/equipe.js
+++ api/routes/equipe.js
@@ -40,52 +40,5 @@ function getequipebyName(req, res) {
         console.log(err))
 }
 
-// Ajout d'un match (POST)
-function postMatchs(req, res) {
-    let match = new Match();
-    /* match.id = req.body.id;
-     match.nom = req.body.nom;
-     match.dateDeRendu = req.body.dateDeRendu;
-     match.rendu = req.body.rendu;
- 
-     console.log("POST assignment reçu :");
-     console.log(assignment)
- 
-     assignment.save( (err) => {
-         if(err){
-             res.send('cant post assignment ', err);
-         }
-         res.json({ message: `${assignment.nom} saved!`})
-     })*/
-}
-
-// Update d'un assignment (PUT)
-function updateMatch(req, res) {
-    /*console.log("UPDATE recu assignment : ");
-    console.log(req.body);
-    Assignment.findByIdAndUpdate(req.body._id, req.body, {new: true}, (err, assignment) => {
-        if (err) {
-            console.log(err);
-            res.send(err)
-        } else {
-          res.json({message: 'updated'})
-        }
-      // console.log('updated ', assignment)
-    });
-*/
-}
-
-// suppression d'un assignment (DELETE)
-function deleteMatch(req, res) {
-
-    Assignment.findByIdAndRemove(req.params.id, (err, assignment) => {
-        if (err) {
-            res.send(err);
-        }
-        res.json({ message: `${assignment.nom} deleted` });
-    })
-}
-
-
 
 module.exports = { getEquipes, postMatchs,getequipebyName, getEquipebyId, updateMatch, deleteMatch };
