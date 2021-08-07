69a437635fd65a0e3ed81d44322e2af770b86f2e
Antsa <vranari@yahoo.fr>
2021-08-07 10:04:03 +0300

    nouveau mdp

--- betproject/betproject/src/app/oublimdp/nouveaumdp/nouveaumdp.component.ts
+++ betproject/betproject/src/app/oublimdp/nouveaumdp/nouveaumdp.component.ts
@@ -3,6 +3,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute, Router } from '@angular/router';
 import { InscriptionComponent } from 'src/app/inscription/inscription/inscription.component';
 import { LoginComponent } from 'src/app/login/login/login.component';
+import { AuthService } from 'src/app/shared/services/auth.service';
 import { MailerService } from 'src/app/shared/services/mailer.service';
 
 @Component({
@@ -17,7 +18,7 @@ export class NouveaumdpComponent implements OnInit {
   hide = true;
   error = "";
   constructor(public dialog: MatDialog, private auth: MailerService, private route: ActivatedRoute,
-    private router: Router) {
+    private router: Router, private authserv:AuthService) {
     // this.realmail = localStorage.getItem('mail');
     //  this.emailhidden = this.censorEmail(this.realmail);
   }
@@ -26,22 +27,29 @@ export class NouveaumdpComponent implements OnInit {
   }
 
   onSubmit($event) {
+    let id=localStorage.getItem('v');
     if (this.password != this.password2) {
       this.error = "Les mots de passes ne sont pas identiques";
     } else {
-      this.auth.resetpass(this.password)
+      this.authserv.resetpass(this.password,id)
         .subscribe(m => {
-          if(m){
-            
+          if(m.msg!="error"){
+            console.log(m +" update mdp");
+            this.openLogin();
+          }else{
+            this.error= " une erreur s'est produite, veuillez recommencer.";
           }
-          console.log("ok");
         })
     }
 
   }
-  openLoginDialog() {
+   openLogin() {
     this.dialog.closeAll();
     const dialogRef = this.dialog.open(LoginComponent);
+  }
+  openSignInDialog() {
+    this.dialog.closeAll();
+    const dialogRef = this.dialog.open(InscriptionComponent);
     dialogRef.afterClosed().subscribe(result => {
       console.log(`Dialog result: ${result}`);
     });
