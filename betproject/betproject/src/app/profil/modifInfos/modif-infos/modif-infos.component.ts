c5b003aec1272f500a7db073399bd9a1eefb680a
Antsa <vranari@yahoo.fr>
2021-08-07 10:04:40 +0300

    modif infos

--- betproject/betproject/src/app/profil/modifInfos/modif-infos/modif-infos.component.ts
+++ betproject/betproject/src/app/profil/modifInfos/modif-infos/modif-infos.component.ts
@@ -1,5 +1,9 @@
+import { DatePipe } from '@angular/common';
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { Parieur } from 'src/app/shared/models/parieur.model';
+import { AuthService } from 'src/app/shared/services/auth.service';
 import { CartService } from 'src/app/shared/services/cart.service';
 
 @Component({
@@ -10,25 +14,51 @@ import { CartService } from 'src/app/shared/services/cart.service';
 export class ModifInfosComponent implements OnInit {
 
 
-  constructor(private cartserv: CartService,
-    private dialogRef: MatDialogRef<ModifInfosComponent>,
-    @Inject(MAT_DIALOG_DATA) data) { }
-
+  constructor(private cartserv: CartService, private router: Router,
+    private dialogRef: MatDialogRef<ModifInfosComponent>, private datePipe: DatePipe,
+    @Inject(MAT_DIALOG_DATA) data, private auth: AuthService) { }
+  user: Parieur = new Parieur();
   nom = null;
-  pseudo=null;
+  pseudo = null;
   prenom = null;
   mail = null;
-  datenaiss=null;
+  datenaiss = null;
   error = "";
+  id = null;
 
   ngOnInit(): void {
+    this.user = JSON.parse(localStorage.getItem('user'))
+    // console.log(this.user.dateNaissance + " MAIL USER")
+    this.id = this.user.id;
+    this.nom = this.user.nom;
+    this.prenom = this.user.prenom;
+    this.pseudo = this.user.pseudo;
+    this.mail = this.user.mail;
+    this.datenaiss = this.user.dateNaissance;
+    this.datenaiss = this.datePipe.transform(this.user.dateNaissance, 'yyyy-MM-dd');
+
   }
 
   onSubmit(event) {
-
-      this.dialogRef.close({ nom: this.nom,pseudo:this.pseudo,prenom:this.prenom,mail:this.mail,datenaiss:this.datenaiss });
+    let newuser = new Parieur();
+    newuser.dateNaissance = this.datenaiss;
+    newuser.nom = this.nom;
+    newuser.prenom = this.prenom;
+    newuser.id = this.id;
+    newuser.mail = this.mail;
+    newuser.pseudo = this.pseudo;
+    this.auth.updateUser(newuser)
+      .subscribe(u => {
+        console.log(u.msg.id + " U ID")
+        if (u.msg.id) {
+          this.dialogRef.close();
+          localStorage.setItem("user", JSON.stringify(u.msg));
+          let currentUrl = this.router.url;
+          this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+          this.router.onSameUrlNavigation = 'reload';
+          this.router.navigate([currentUrl]);
+        }
+      })
   }
-updateinfo(){
-  
-}
+
 }
