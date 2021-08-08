b1661ca4fc270f873f19eeccb22600ffbd7131c7
Antsa <vranari@yahoo.fr>
2021-08-08 23:03:41 +0300

    controles de valeurs et pop up

---
+++ src/app/coupons/checkjetons/checkmise/checkmise.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-checkmise',
+  templateUrl: './checkmise.component.html',
+  styleUrls: ['./checkmise.component.css']
+})
+export class CheckmiseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
